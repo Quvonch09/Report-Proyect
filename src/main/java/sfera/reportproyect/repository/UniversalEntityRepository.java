5ae9cef4a388e0945bf786a2ea3526092b1a3ce4
Muhammad <muhammadnazirqulov1@gmail.com>
2026-02-09 16:52:00 +0500

    1

--- src/main/java/sfera/reportproyect/repository/UniversalEntityRepository.java
+++ src/main/java/sfera/reportproyect/repository/UniversalEntityRepository.java
@@ -1,9 +1,25 @@
 package sfera.reportproyect.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sfera.reportproyect.entity.UniversalEntity;
+import sfera.reportproyect.entity.enums.TypeEnum;
 
 @Repository
 public interface UniversalEntityRepository extends JpaRepository<UniversalEntity, Long> {
+    boolean existsByTypeEnumAndNameIgnoreCase(TypeEnum typeEnum, String name);
+
+    @Query("""
+        SELECT u FROM UniversalEntity u
+        WHERE u.typeEnum = :typeEnum
+          AND (:name IS NULL OR TRIM(:name) = '' OR LOWER(u.name) LIKE LOWER(CONCAT('%', :name, '%')))
+    """)
+    Page<UniversalEntity> search(@Param("typeEnum") TypeEnum typeEnum,
+                                 @Param("name") String name,
+                                 Pageable pageable);
+
 }
--- src/main/java/sfera/reportproyect/service/UniversalEntityService.java
+++ src/main/java/sfera/reportproyect/service/UniversalEntityService.java
@@ -1,9 +1,13 @@
 package sfera.reportproyect.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import sfera.reportproyect.dto.ApiResponse;
 import sfera.reportproyect.dto.request.req.ReqFilial;
+import sfera.reportproyect.dto.request.res.ResPageable;
 import sfera.reportproyect.entity.UniversalEntity;
 import sfera.reportproyect.entity.enums.TypeEnum;
 import sfera.reportproyect.repository.UniversalEntityRepository;
@@ -14,12 +18,37 @@ public class UniversalEntityService {
     private final UniversalEntityRepository universalEntityRepository;
 
     public ApiResponse<String> save(ReqFilial reqFilial, TypeEnum typeEnum) {
-        UniversalEntity universalEntity = UniversalEntity.builder()
-                .name(reqFilial.getName())
-                .active(true)
-                .typeEnum(typeEnum)
+        if (reqFilial.getName() == null || reqFilial.getName().isEmpty()) {
+            return ApiResponse.error("name cannot be empty");
+        }
+
+        boolean exists = universalEntityRepository.existsByTypeEnumAndNameIgnoreCase(typeEnum, reqFilial.getName());
+        if (exists) {
+            return ApiResponse.error("This name already exists");
+        }
+
+            UniversalEntity universalEntity = UniversalEntity.builder()
+                    .name(reqFilial.getName())
+                    .active(true)
+                    .typeEnum(typeEnum)
+                    .build();
+            universalEntityRepository.save(universalEntity);
+            return ApiResponse.success(null, "Success");
+    }
+
+    public ApiResponse<ResPageable> getByPage(TypeEnum typeEnum ,String name, int page,int size){
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by("id").descending());
+        Page<UniversalEntity> search = universalEntityRepository.search(typeEnum, name, pageRequest);
+
+
+        ResPageable resPageable = ResPageable.builder()
+                .page(page)
+                .size(size)
+                .totalElements()
+                .totalPage()
+                .body()
                 .build();
-        universalEntityRepository.save(universalEntity);
-        return ApiResponse.success(null,"Success");
+        return ApiResponse.success(resPageable, "Success");
+
     }
 }
